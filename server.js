e1b3f27dfe8a093894617557270ece8ab1fbd976
luispafsolucoes <luispafsolucoes@gmail.com>
2023-02-12 14:10:03 -0300

    ajustes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 // iniciar express
 const app = express();
 // nome da pasta no dist que sera feito o build
-const appName = 'luispaf_front';
+const appName = 'frontend';
 // local onde build ira gerar os arquivos
 const outputPath = `${__dirname}/dist/${appName}`;
 
@@ -11,7 +11,7 @@ const outputPath = `${__dirname}/dist/${appName}`;
 app.use(express.static(outputPath));
 // redirecionar qualquer requisicao para o index.html
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + "/dist/frontend/index.html");
+  res.sendFile(`${outputPath}/index.html`);
 });
 // ouvir a porta que o Heroku disponibilizar
 app.listen(process.env.PORT);
